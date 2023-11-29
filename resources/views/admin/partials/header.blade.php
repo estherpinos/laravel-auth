172cc4926aa46bc7a829860e1d782f4718ca3054
estherpinos <estherpinos1@gmail.com>
2023-11-29 17:36:47 +0100

    Add [files]

---
+++ resources/views/admin/partials/header.blade.php
@@ -0,0 +1,15 @@
+<header class="bg-dark">
+    <nav class="navbar-dark navbar">
+        <div class="container-fluid">
+            <a class="navbar-brand">vai al sito</a>
+            <form class="d-flex" role="search" method="POST">
+                @csrf
+                <button class="btn btn-light" type="submit"></button>
+            </form>
+
+
+        </div>
+
+    </nav>
+
+</header>
---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,8 @@
+<aside>
+    <nav>
+        <ul>
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#">Lista</a></li>
+        </ul>
+    </nav>
+</aside>
