93a1d0aa01cc7f0729ebe8683e2ab3e43bdd9584
estherpinos <estherpinos1@gmail.com>
2023-11-30 15:10:13 +0100

    Add [controllers]

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside>
     <nav>
         <ul>
-            <li><a href="#">Dashboard</a></li>
+            <li><a href="{{route('admin.home')}}">Dashboard</a></li>
             <li><a href="#">Lista</a></li>
         </ul>
     </nav>
